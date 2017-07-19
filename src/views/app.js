7878bba7f9311898e861bee43abf5cbe92d2563e
Hack Reactor Student <student@hackreactor.com>
2017-07-18 19:40:47 -0700

    Implemented dynamic videoPlayerView

--- src/views/app.js
+++ src/views/app.js
@@ -4,9 +4,13 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos(window.exampleVideoData);
+    
+     
 
     this.render();
   },
+
+  
 
   render: function() {
     this.$el.html(this.template());
@@ -17,7 +21,18 @@ var AppView = Backbone.View.extend({
       collection: this.videos,
     }).render();
     
-    
+    new SearchView({
+      el: this.$('.search'),
+    }).render();
+
+    new VideoPlayerView({
+      el: this.$('.player'),
+      //In order to pass VideoPlayerView one model
+      //does one have to also pass a collection? prollllyyy 
+      model: this.videos.at(0),
+      collection: this.videos,
+      
+    }).render();
   },
 
   template: templateURL('src/templates/app.html')
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,9 +1,15 @@
 var VideoListEntryView = Backbone.View.extend({
 
-  
+  event: {
+    'click .video-list-entry-title': 'onTitleClick'
+  },  
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
+  },
+  
+  onTitleClick: function() {
+    
   },
 
   //
